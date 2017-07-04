d2ecd655d11b4379f266bdd0398b5aa5b6e0d03a
Phumlile Zamela <rhodakadaddy@gmail.com>
2017-07-04 18:58:55 +0200

    Slider Widget

--- gui_test.py
+++ gui_test.py
@@ -1,20 +1,23 @@
-from guizero import App, Text, TextBox, PushButton
+from guizero import App, Text, TextBox, PushButton, Slider
 
 def say_my_name():
-    currentText = my_name.get()
-    welcome_message.set(currentText)
-    new_color = my_color.get()
-    welcome_message.color(new_color)
-    new_size = int(my_size.get())
-    welcome_message.font_size(new_size)
-    
-app = App(title="Hello World", height=400, width=400)
+    welcome_message.set( my_name.get() )
+    welcome_message.color( my_color.get() )
+    welcome_message.font_size( my_size.get() )
 
-welcome_message = Text(app, text="Welcome to my app", size=20, font="Times New Roman", color="#0000ff")
-my_name = TextBox(app, width=30)
+def change_text_size(slider_value):
+    welcome_message.font_size(slider_value)
+    my_size.set(slider_value)
+
+windowSize = 600
+app = App(title="Hello World", height=windowSize, width=windowSize)
+
+welcome_message = Text(app, text="Welcome to my app", size=10, font="Times New Roman", color="#0000ff")
+my_name = TextBox(app, width=30, text=welcome_message.get())
 my_color = TextBox(app, width=30, text="red")
 my_size = TextBox(app, width=30, text="30")
 update_text = PushButton(app, command=say_my_name, text="Display my name")
+text_size = Slider(app, command=change_text_size, start=10, end=52)
  
 app.display()
 
