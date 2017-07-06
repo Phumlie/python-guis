3f1dc6269191d13e88e6a55b6e462cee227348c6
Phumlile Zamela <rhodakadaddy@gmail.com>
2017-07-06 13:36:28 +0200

    Combo widget.

--- gui_test2.py
+++ gui_test2.py
@@ -3,6 +3,6 @@ from guizero import App, Combo, Text
 app = App(title="My second GUI app", width=700, height=700, layout="grid")
 
 film_choice = Combo(app, options=["None", "Pasta", "Rice", "Couscous"], grid=[0,1], align="left")
-film_description = Text(app, text="Which food is your favourate?", grid=[0,0], align="left")
+film_description = Text(app, text="What is your favourite food?", grid=[0,0], align="left")
 
 app.display()
