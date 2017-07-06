93f8d5c4bacef9c5487d4a1a9b92e08f1bf49093
Phumlile Zamela <rhodakadaddy@gmail.com>
2017-07-06 13:26:07 +0200

    Combo Widgetgit add .

--- gui_test2.py
+++ gui_test2.py
@@ -1,4 +1,8 @@
-from guizero import App
+from guizero import App, Combo, Text
 
-app = App(title="My second GUI app", width=300, height=200, layout="grid")
+app = App(title="My second GUI app", width=700, height=700, layout="grid")
+
+film_choice = Combo(app, options=["None", "Pasta", "Rice", "Couscous"], grid=[0,1], align="left")
+film_description = Text(app, text="Which food is your favourate?", grid=[0,0], align="left")
+
 app.display()
